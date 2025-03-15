9b96cccca96f40de9bb351c52b7b2fddc2e3a5b0
MikolajNiewola <mikolaj.niewola.321@gmail.com>
2025-03-15 20:32:11 +0100

    new styles and display of chosen items

--- mobdle/src/components/gamePage/GamePage.jsx
+++ mobdle/src/components/gamePage/GamePage.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Select from 'react-select';
+import './GamePage.css'
 
-const options = [
-  { value: 'chocolate', label: 'Chocolate', image: 'https://minecraft.wiki/images/ZombieFace.png?d1bba'},
-  { value: 'strawberry', label: 'Strawberry', image: 'https://minecraft.wiki/images/EndermanFace.png?8ebeb' },
-  { value: 'vanilla', label: 'Vanilla', image: 'https://minecraft.wiki/images/SkeletonFace.png?652cd' }
+const initialOptions = [
+  { name: 'Zombie', image: 'https://minecraft.wiki/images/ZombieFace.png?d1bba', version: 'pre-alpha', health: 20, height: 1.95, behavior: ['Hostile'], movement: ['Walking '], dimension: ['Overworld ']},
+  { name: 'Enderman', image: 'https://minecraft.wiki/images/EndermanFace.png?8ebeb', version: 'Beta 1.8', health: 200, height: 2.9, behavior: ['Neutral'], movement: ['Walking ','Teleportation '], dimension: ['End ', 'Overworld ', 'Nether ']},
 ];
-  
 
 const customStyles = {
     
-    container: (styles) => ({
-        ...styles,
-        width: window.innerWidth < 768 ? '80%' : '60%',
-        margin: '0 auto',
-        marginTop:  window.innerWidth < 768 ? '2vh' : '5vh' 
-      }),
+  container: (styles) => ({
+      ...styles,
+      width: '100%',
+      margin: '0 auto',
+  }),
+
   control: (styles) => ({
     ...styles,
     backgroundColor: '#a0c4ff',
@@ -23,9 +22,10 @@ const customStyles = {
     padding: '4px',
     color:'#F0F',
     borderColor: '#a0c4ff',
-    fontSize: window.innerWidth < 768 ? '2.5vh' : '3vh',
-    
+    fontSize: '3vh',
+    cursor: 'pointer',
   }),
+
   option: (styles, { isDisabled, isFocused, isSelected }) => ({
     ...styles,
     display: 'flex',
@@ -34,12 +34,12 @@ const customStyles = {
       ? '#0c1a2b'
       : '#a0c4ff',
     cursor: isDisabled ? 'not-allowed' : 'pointer',
-    fontSize: window.innerWidth < 768 ? '2.5vh' : '3vh',
-
+    fontSize: '3vh',
     ':active': {
       backgroundColor: '#ddd',
     },
   }),
+
   singleValue: (styles) => ({
     ...styles,
     display: 'flex',
@@ -49,41 +49,79 @@ const customStyles = {
 
   menu: (styles) => ({
     ...styles,
-    borderRadius: '0px', // Bez zaokrągleń
-    overflow: 'hidden', // Ukryj białe kreski
+    borderRadius: '0px',
+    overflow: 'hidden',
     marginTop: '2px',
-    backgroundColor: '#a0c4ff', // Dopasuj tło
+    backgroundColor: '#a0c4ff',
   }),
-
-
 
 };
 
-const formatOptionLabel = ({ label, image }) => (
+const formatOptionLabel = ({ name, image }) => (
   <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
     <img
       src={image}
-      alt={label}
+      alt={name}
       style={{ width: 24, height: 24, borderRadius: 4 }}
     />
-    {label}
+    {name}
   </div>
 );
+
 function GamePage() {
-        return (
-                <Select options={options}
-                    styles={customStyles}
-                    formatOptionLabel={formatOptionLabel}
-                    placeholder="Wybierz opcję..."
-                    components={{
-                        DropdownIndicator:() => null,
-                        IndicatorSeparator:() => null,
-                    }}
-                    isClearable={true}
-                    >
-                    
-                </Select>
-        )
+  const [options, setOptions] = useState(initialOptions);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [chosen, setChosen] = useState([]); // do poprawy
+
+  // cała funkcja do poprawy wsm
+  const handleSubmit = () => {
+    if(selectedOption) {
+      setChosen((prevChosen) => [selectedOption, ...prevChosen])
+      setOptions((prevOptions) => prevOptions.filter(option => option.name !== selectedOption.name));
+      setSelectedOption(null);
+      console.log(chosen);
     }
+  }  
+
+  return (
+    <div className='gp_mainContainer'>
+      <div className='gp_guessingZone'>
+        <Select options={options}
+            styles={customStyles}
+            formatOptionLabel={formatOptionLabel}
+            placeholder="Wybierz opcję..."
+            components={{
+                DropdownIndicator:() => null,
+                IndicatorSeparator:() => null,
+            }}
+            isClearable={true}
+            value={selectedOption}
+            onChange={setSelectedOption}
+            key={options.length}
+        />
+        <button onClick={handleSubmit}>{'>>'}</button>
+      </div>
+      
+
+      <div className='gp_chosenOptions'>
+          <h2>Wybrane:</h2>
+          {chosen.map((item, index) => (
+          <div key={index} className='gp_chosenContainer'>
+            <img src={item.image} alt={item.name} />
+            <div>{item.version}</div>
+            <div>{item.health}</div>
+            <div>{item.height}</div>
+            <div>{item.behavior}</div>
+            <div>{item.movement}</div>
+            <div>{item.dimension}</div>
+          </div>
+        ))}
+          
+      </div>
+
+
+    </div>
+  )
+}
 
 export default GamePage
