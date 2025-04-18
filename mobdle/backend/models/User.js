42e3c89f2c79f76058051ec0743844875399d9a6
Mori656 <marcelnosek@o2.pl>
2025-04-18 18:21:26 +0200

    fix css

--- mobdle/backend/models/User.js
+++ mobdle/backend/models/User.js
@@ -27,7 +27,7 @@ const userSchema = new mongoose.Schema({
         type: Date,
         required: true,
     }
-}, { timestamps: true });
+});
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
